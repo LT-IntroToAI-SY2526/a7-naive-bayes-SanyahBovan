97ea0f245b73e9dfa20289cb77d400e8a261347e
Sanyah Bovan <sbovan1@cps.edu>
2026-04-10 10:28:48 -0500

    update dict

--- a7.py
+++ a7.py
@@ -87,8 +87,11 @@ class BayesClassifier:
         # positive frequency dictionary. If it is neither a postive or negative file,
         # ignore it and move to the next file (this is more just to be safe; we won't
         # test your code with neutral reviews)
-        
-
+            if filename.startswith(self.neg_file_prefix):
+                self.update_dict(tokens, self.neg_freqs)
+            elif filename.startswith(self.pos_file_prefix):
+                self.update_dict(tokens, self.pos_freqs)
+        print(self.neg_freqs)
         # Updating frequences: to update the frequencies for each file, you need to get
         # the text of the file, tokenize it, then update the appropriate dictionary for
         # those tokens. We've asked you to write a function `update_dict` that will make
